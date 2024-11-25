e0f680cf5fd8f2b45ed7bc2fa22ec8b3e3fbcd02
Valeria Marin <valeriamarin193@gmail.com>
2024-11-25 08:13:29 -0500

    solved even odd second time

--- elememts_of_programming_interviews_in_py/arrays/even_odd.py
+++ elememts_of_programming_interviews_in_py/arrays/even_odd.py
@@ -1,6 +1,15 @@
 # Reorder the input array so that the even entries appear first. 
 # Solve in O(1) space
 
+def even_odd2(A):
+    next_even, next_odd = 0, len(A) - 1
+    if next_even % 2 == 0:
+        next_even += 1
+    else:
+        A[next_even], A[next_odd] = A[next_odd], A[next_even] 
+        next_odd -= 1
+    return A
+
 # T: O(N), S: O(1)
 def even_odd(A):
     # With arrays is easy to operate on both ends
@@ -24,3 +33,4 @@ arr = [2,5,6,7,7,777,8,1,2,34,5,80,66,3,77,98,8,7,68,8]
 even_odd(arr)
 assert arr == [2, 8, 6, 68, 8, 98, 8, 66, 2, 34, 80, 5, 3, 77, 1, 777, 7, 7, 7, 5], \
     f"not correctly sorted, got: {arr}"
+print(even_odd2(arr))
