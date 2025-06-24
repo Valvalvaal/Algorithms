a757eb3c968052dd763c5c0c84e99034fca5de57
Valeria <valeriamarin193@gmail.com>
2025-06-23 22:10:42 -0500

    upload solution

---
+++ educative/remove_nth_node_from_end_of_list.py
@@ -0,0 +1,31 @@
+# Definition for a Linked List node
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from LinkedList.LinkedList import ListNode
+
+def remove_nth_last_node(head, n):
+    
+    # For my solution, I'll use two pointers to traverse the entire linked list. 
+    # The pointers will be n steps away from each other. That way, when the pointer
+    # that's ahead reaches the end of the array, the remaining pointer will be on the
+    # node I want to remove.
+    p1 = head
+    p2 = head
+    
+    while n:
+        p1 = p1.next
+        n -= 1
+        
+    if not p1:
+        return head.next
+    
+    while p1.next:
+        p1 = p1.next
+        p2 = p2.next
+    
+    p2.next = p2.next.next
+        
+    return head
+
