dc30445b9d59fde0d607a7abe6fdb8c8ba5f46f7
Valeria <valeriamarin193@gmail.com>
2025-06-24 10:00:00 -0500

    upload solution

--- educative/remove_nth_node_from_end_of_list.py
+++ educative/remove_nth_node_from_end_of_list.py
@@ -1,31 +1,40 @@
 # Definition for a Linked List node
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-from ds_v1.LinkedList.LinkedList import ListNode
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
+# T: O(N), S:O(1)
 def remove_nth_last_node(head, n):
     
     # For my solution, I'll use two pointers to traverse the entire linked list. 
     # The pointers will be n steps away from each other. That way, when the pointer
     # that's ahead reaches the end of the array, the remaining pointer will be on the
     # node I want to remove.
-    p1 = head
-    p2 = head
     
+    # define pointers at the beginning of the list
+    fast = head
+    slow = head
+    
+    # Fast will be n steps ahead of slow
     while n:
-        p1 = p1.next
+        fast = fast.next
         n -= 1
-        
-    if not p1:
+    
+    # If fast is already after the last node, it means the slow pointer is already
+    # standing on the slow node, so we return the next node from the original head as
+    # the head node for the return list. 
+    if not fast: 
         return head.next
     
-    while p1.next:
-        p1 = p1.next
-        p2 = p2.next
+    # Continue traversing the list until we get to the last node   
+    while fast.next:
+        fast = fast.next
+        slow = slow.next
     
-    p2.next = p2.next.next
+    # Slow pointer points at the node n+1 from the end, so we eliminate the node n 
+    # steps from the end by jumping over it and pointing to the next.next node
+    slow.next = slow.next.next    
         
     return head
 
