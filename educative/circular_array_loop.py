4add9d40ba9a20161406644045a4af2e292b67a2
Valeria Marin <valeriamarin193@gmail.com>
2025-07-02 00:02:18 -0500

    naive solution

--- educative/circular_array_loop.py
+++ educative/circular_array_loop.py
@@ -1,9 +1,10 @@
 def circular_array_loop(nums):  
-    arr_len = len(arr)
-    visited = {}
+    arr_len = len(nums)
+    visited = {0}
     idx = 0
     
     while arr_len:
+        print(idx)
         idx += nums[idx]
         if idx in visited:
             return True
